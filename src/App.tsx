3575a3b5e6e244678917906e60c50605acfcc243
ezurno <ezurno@gmail.com>
2023-02-10 13:27:48 +0900

    LEARN: how to use framer-motion
    1. framer-motion으로 animate 할 떈 tag를 <motion.tagName> 으로 해야함
    
    2. styled-components로 같이 쓸 땐 styled(mtion.div)로 사용
    
    3. tag 내 props를 수정하면 animate 가능

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -9,18 +10,24 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
-  border-radius: 10px;
+  border-radius: 25px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box />
+      <Box
+        transition={{ type: "spring", damping: 10, delay: 0.5 }}
+        // defalut type은 spring, dampling 은 물리 저항력, 0은 저항이 없어서 계속 반복함
+        // mass 는 무게, bounce는 튕기는 양
+        initial={{ scale: 0 }}
+        animate={{ scale: 1, rotateZ: 360 }}
+      />
     </Wrapper>
   );
 }
