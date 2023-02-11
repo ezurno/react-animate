b768a182afe9672fbb190f4e8d261b7ef5ca2ad1
ezurno <ezurno@gmail.com>
2023-02-11 22:50:49 +0900

    LEARN: use drag-props
    1.
     - dragElastic => drag 시 탄성
     - dragConstraints => dragBox 조정
     - dragSnapToOrigin => drag 종료 시 원점 위치로 복귀 (boolean)
    
    2. useRef를 사용해 간단하게 지정 가능

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import styled from "styled-components";
 import { Variants, motion } from "framer-motion";
 
@@ -18,27 +18,38 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const BiggerBox = styled.div`
+  width: 600px;
+  height: 600px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 25px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+`;
+
 const BoxVarient: Variants = {
-  hover: { scale: 1.5, rotateZ: 90 },
-  click: { scale: 1, borderRadius: "100px" },
-  drag: {
-    backgroundColor: "#123456",
-    transition: {
-      duration: 3,
-    },
-  },
+  hover: { rotateZ: 90 },
+  click: { borderRadius: "100px" },
 };
 
 function App() {
+  const biggerBoxRef = useRef<HTMLDivElement>(null);
   return (
     <Wrapper>
-      <Box
-        variants={BoxVarient}
-        drag
-        whileDrag={"drag"}
-        whileHover="hover"
-        whileTap="click"
-      />
+      <BiggerBox ref={biggerBoxRef}>
+        <Box
+          drag
+          // dragConstraints={{ top: 0, bottom: 0, left: 0, right: 0 }} 일반적인 방식
+          dragElastic={0} // default Value = 0.5 drag 시 탄성 조절
+          dragConstraints={biggerBoxRef} // dragBox 의 틀을 설정 useRef로 지정
+          dragSnapToOrigin={false} // drag가 끝날 시 원점으로 돌아가는지
+          variants={BoxVarient}
+          whileHover="hover"
+          whileTap="click"
+        />
+      </BiggerBox>
     </Wrapper>
   );
 }
