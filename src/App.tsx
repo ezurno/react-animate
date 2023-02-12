2a70e446accbf7b5123cdf87384641bc7cd0a1d8
ezurno <ezurno@gmail.com>
2023-02-12 20:02:15 +0900

    LEARN: svg animation
    1. https://fontawesome.com/v5.15/icons/airbnb?style=brands
    
    2. transition을 따로 주려면 motion.path 의 props로 default와 따로 줄 값을 입력
    
    3. pathLength와 pathSpacing, pathOffset같은 animate 가 있으므로 참고
     - https://www.framer.com/docs/examples/#line-drawing
    
    4. props d 를 SvgVarient의 start에 d로 넣어도 됨

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import {
 } from "framer-motion";
 
 const Wrapper = styled(motion.div)`
-  height: 200vh;
+  height: 100vh;
   width: auto;
   display: flex;
   justify-content: center;
@@ -43,37 +43,48 @@ const BoxVarient: Variants = {
   click: { borderRadius: "100px" },
 };
 
+const Svg = styled.svg`
+  width: 300px;
+  height: 300px;
+  path {
+    stroke: white; // 겉 테두리만
+    stroke-width: 2; // 테두리 크기
+  }
+`;
+
+const SvgVarient: Variants = {
+  start: {
+    pathLength: 0,
+    fill: "rgba(255,255,255,0)",
+  },
+  end: {
+    fill: "rgba(255,255,255,1)",
+    pathLength: 1,
+  },
+};
+
 function App() {
   const wrapperRef = useRef<HTMLDivElement>(null);
-  const x = useMotionValue(0); // animation의 state와 velocity를 추적함 => react에서 자동으로 rerendering 하지 않게 block함
-  const rotateZ = useTransform(x, [-800, 0, 800], [-360, 0, 360]); // useTransfom 은 한 값을 구간을 나눠 값을 자동 치환 해줌
-  const gradient = useTransform(
-    x,
-    [-800, 0, 800],
-    [
-      "linear-gradient(135deg, #1800ee, #00a3ee)",
-      "linear-gradient(135deg, #e09, #d0e)",
-      "linear-gradient(135deg, #2cee00, #beee00)",
-    ]
-  );
-
-  const { scrollY, scrollYProgress } = useScroll();
-  useEffect(() => {
-    scrollY.onChange(() => {
-      console.log(scrollY.get(), scrollYProgress.get()); // scrollY 와 scrollYProcss의 값은 각각 pixel, main % 값임
-    });
-  }, [scrollY, scrollYProgress]);
-
-  const scale = useTransform(scrollYProgress, [0, 1], [1, 5]);
 
   return (
-    <Wrapper ref={wrapperRef} style={{ background: gradient }}>
-      <Box
-        drag="x"
-        dragSnapToOrigin
-        style={{ x, rotateZ, scale }}
-        dragConstraints={wrapperRef}
-      />
+    <Wrapper ref={wrapperRef}>
+      <Svg
+        focusable="false"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 448 512"
+      >
+        <motion.path // path를 motion.path로 수정해 주어야 animate 사용 가능
+          variants={SvgVarient}
+          initial="start"
+          animate="end"
+          transition={{
+            // transition을 밖에다 주어 default로 기본값을 부여, 따로 fill을 transition 부여
+            default: { duration: 5 },
+            fill: { duration: 1, delay: 3 },
+          }}
+          d="M224 373.12c-25.24-31.67-40.08-59.43-45-83.18-22.55-88 112.61-88 90.06 0-5.45 24.25-20.29 52-45 83.18zm138.15 73.23c-42.06 18.31-83.67-10.88-119.3-50.47 103.9-130.07 46.11-200-18.85-200-54.92 0-85.16 46.51-73.28 100.5 6.93 29.19 25.23 62.39 54.43 99.5-32.53 36.05-60.55 52.69-85.15 54.92-50 7.43-89.11-41.06-71.3-91.09 15.1-39.16 111.72-231.18 115.87-241.56 15.75-30.07 25.56-57.4 59.38-57.4 32.34 0 43.4 25.94 60.37 59.87 36 70.62 89.35 177.48 114.84 239.09 13.17 33.07-1.37 71.29-37.01 86.64zm47-136.12C280.27 35.93 273.13 32 224 32c-45.52 0-64.87 31.67-84.66 72.79C33.18 317.1 22.89 347.19 22 349.81-3.22 419.14 48.74 480 111.63 480c21.71 0 60.61-6.06 112.37-62.4 58.68 63.78 101.26 62.4 112.37 62.4 62.89.05 114.85-60.86 89.61-130.19.02-3.89-16.82-38.9-16.82-39.58z"
+        />
+      </Svg>
     </Wrapper>
   );
 }
