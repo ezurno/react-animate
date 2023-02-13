fa3664b4f2fa5d4c3b7d24fde15f74752b40aa8b
ezurno <ezurno@gmail.com>
2023-02-14 01:21:28 +0900

    LEARN: layout animate
    1. layout이 변경 되는 animation을 만들 경우 parent tag의 style props를 값을 받아와 수정
    
    2. children tag의 props로 layout을 넣으면 자동으로 animation 생성

--- src/App.tsx
+++ src/App.tsx
@@ -30,78 +30,35 @@ const Box = styled(motion.div)`
   position: absolute;
   top: 100px;
   display: flex;
-  align-items: center;
-  justify-content: center;
+
   font-size: 28px;
 `;
 
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.2);
+const Circle = styled(motion.div)`
+  width: 50px;
+  height: 50px;
   border-radius: 25px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
+  background-color: rgba(35, 103, 221, 0.8);
 `;
 
-const BoxVarient: Variants = {
-  initial: (back: Boolean) => ({
-    opacity: 0,
-    scale: 0,
-    x: back ? -300 : 300,
-  }),
-
-  animate: {
-    x: 0,
-    scale: 1,
-    opacity: 1,
-    transition: {
-      duration: 1,
-    },
-  },
-
-  exit: (back: Boolean) => ({
-    x: back ? 300 : -300,
-    scale: 0,
-    opacity: 0,
-    transition: {
-      duration: 1,
-    },
-  }),
-};
-
 function App() {
-  const [pageNumber, setPageNumber] = useState(1);
-  const [back, setBack] = useState(false);
-
-  const onNext = async () => {
-    await setBack(false);
-    setPageNumber((page) => (page === 10 ? 10 : page + 1));
-  };
-  const onPrev = async () => {
-    await setBack(true);
-    // code 엔 영향을 주지 않지만 set이 실행 되기 전에 setPageNumber가 먼저 실행 되어 방향이 뒤틀리게 됨 => 비동기식 처리로 해결
-    setPageNumber((page) => (page === 1 ? 1 : page - 1));
+  const [touch, setTouch] = useState(false);
+  const onClick = () => {
+    setTouch((current) => !current);
   };
 
   return (
     <Wrapper>
-      <AnimatePresence mode="wait" custom={back}>
-        <Box
-          custom={back}
-          variants={BoxVarient}
-          initial="initial"
-          exit="exit"
-          animate="animate"
-          key={pageNumber}
-        >
-          {pageNumber}
-        </Box>
-      </AnimatePresence>
-      <button onClick={onNext}>Next</button>
-      <button onClick={onPrev}>Prev</button>
+      <Box
+        style={{
+          justifyContent: touch ? "center" : "flex-start",
+          alignItems: touch ? "center" : "flex-start",
+        }}
+      >
+        <Circle layout />
+      </Box>
+
+      <button onClick={onClick}>Click me</button>
     </Wrapper>
   );
 }
