59dc8ffff0a30ac9272b1dae4797dbba3f9c6dd6
ezurno <ezurno@gmail.com>
2023-02-14 01:28:32 +0900

    LEARN: how to use layoutId props
    1. layoutId를 부여해 layout의 이름을 따로 지정 가능
    
    2. layoutId를 사용 하면 layout의 id를 지정하는 것 이며 layout이 변할 시 animate를 줌

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,11 @@ const Wrapper = styled(motion.div)`
   height: 100vh;
   width: auto;
   display: flex;
-  flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: space-around;
   background: linear-gradient(135deg, #e09, #d0e);
   overflow: hidden;
+  gap: 50px;
 `;
 
 const Box = styled(motion.div)`
@@ -27,10 +27,10 @@ const Box = styled(motion.div)`
   background-color: rgba(255, 255, 255, 1);
   border-radius: 25px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  position: absolute;
   top: 100px;
   display: flex;
-
+  justify-content: center;
+  align-items: center;
   font-size: 28px;
 `;
 
@@ -48,17 +48,9 @@ function App() {
   };
 
   return (
-    <Wrapper>
-      <Box
-        style={{
-          justifyContent: touch ? "center" : "flex-start",
-          alignItems: touch ? "center" : "flex-start",
-        }}
-      >
-        <Circle layout />
-      </Box>
-
-      <button onClick={onClick}>Click me</button>
+    <Wrapper onClick={onClick}>
+      <Box>{touch ? <Circle layoutId="blueBall" /> : null}</Box>
+      <Box>{touch ? null : <Circle layoutId="blueBall" />}</Box>
     </Wrapper>
   );
 }
