714a574d9364d40f2a91a85da3913f5731d2b9e5
ezurno <ezurno@gmail.com>
2023-02-15 23:05:11 +0900

    SET: set default

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,14 @@
-import React, { useEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
 import styled from "styled-components";
-import {
-  Variants,
-  motion,
-  useMotionValue,
-  useTransform,
-  useViewportScroll,
-  useScroll,
-  AnimatePresence,
-  animate,
-} from "framer-motion";
 
-const Wrapper = styled(motion.div)`
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-  background: linear-gradient(135deg, #e09, #d0e);
-  overflow: hidden;
-`;
+const Wrapper = styled(motion.div)``;
 
-const Box = styled(motion.div)`
-  height: 200px;
-  background-color: rgba(255, 255, 255, 1);
-  border-radius: 25px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  top: 100px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 28px;
-`;
-
-const Grid = styled(motion.div)`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  width: 50vw;
-  gap: 10px;
-
-  div:first-child,
-  div:last-child {
-    grid-column: span 2;
-  }
-`;
-
-const Overlay = styled(motion.div)`
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: absolute;
-`;
+const Nav = styled(motion.div)``;
 
 function App() {
-  const [id, setId] = useState<null | string>(null);
   return (
     <Wrapper>
-      <Grid>
-        {["1", "2", "3", "4"].map((number) => (
-          <Box onClick={() => setId(number)} key={number} layoutId={number} />
-        ))}
-      </Grid>
-      <AnimatePresence>
-        {id ? (
-          <Overlay
-            onClick={() => setId(null)}
-            initial={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
-            animate={{ backgroundColor: "rgba(0, 0, 0,0.5)" }}
-            exit={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
-          >
-            <Box layoutId={id} style={{ width: 400 }} />
-          </Overlay>
-        ) : null}
-      </AnimatePresence>
+      <Nav></Nav>
     </Wrapper>
   );
 }
